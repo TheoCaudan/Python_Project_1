ca9c386ffb13d94303d9e813fbe0341a7da786e8
TheoCaudan <theocaudan38@gmail.com>
2024-10-22 15:33:18 +0200

    Implementation w/o Nmap tools

--- course/main.py
+++ course/main.py
@@ -1,5 +1,87 @@
 import json
 import argparse
+import nmap
+
+configurations = []
+#add_config function
+def add_config():
+    print("Vous êtes dans le menu d'ajout de configuration \n")
+    server_name = input("Entrez le nom du serveur: ")
+    IP_address = input("Entrez l'adresse IP: ")
+    os = input("Entrez le système d'exploitation: ")
+    services = input("Entrez les services en cours d'exécution (séparés par des virgules): ").split(",")
+
+    configuration = {
+        "server_name": server_name,
+        "IP_address": IP_address,
+        "os": os,
+        "services": [services.strip() for service in services]
+    }
+
+    configurations.append(configuration)
+
+#modify_config function
+def mod_config():
+    print("Vous êtes dans le menu de modification de configuration \n")
+    new_server_name = input("Entrez le nom du serveur: ")
+    for config in configurations:
+        if(config["server_name"] == new_server_name):
+            new_IP_address = input("Entrez la nouvelle IP pour {new_IP_address}")
+            config["IP_address"] = new_IP_address
+            print(f'Configuration mise à jour pour {new_server_name}!')
+            return
+        print(f'Aucune configuration trouvée pour {new_server_name}.')
+
+#delete_config function
+def del_config():
+    print("Vous êtes dans le menu de suppression de configuration \n")
+    new_server_name = input("Entrez le nom du serveur à supprimer: ")
+    configurations = [config for config in configurations if config["server_name"]!= new_server_name]
+    print(f'Configuration supprimée pour {new_server_name}!')
+            
+#list_config function
+def ls_config():
+    print("Vous êtes dans le menu de listage de configuration \n")
+    if not configurations:
+        print("Aucune configuration enregistrée.")
+        return
+    print("Configurations enregistrées: \n")
+    for i, config in enumerate(configurations, 1):
+        print(f'{i}. {config["server_name"]}')
+        print(f" -Adresse IP: {config['IP_address']}")
+        print(f" -Système d'exploitation: {config['os']}")
+        print(f" -Services: {','.join(config['services'])}")
+
+#save_config function
+def sav_config():
+    print("Vous êtes dans le menu de sauvegarde de configuration: \n")
+    file = input("Entrez le nom du fichier de sauvegarde (ex: configurations.json): ")
+    with open(file, 'w') as f:
+        json.dump(configurations, f)
+    print(f"Configurations sauvegardées avec succès dans {file}!")
+
+#rollback_config function
+def rol_config():
+    print("Vous êtes dans le menu de restauration de configuration: \n")
+    file = input("Entrez le nom du fichier de sauvegarde à restaurer (ex: configurations.json): ")
+    with open(file, 'r') as f:
+        configurations = json.load(f)
+    print(f"Configurations restaurées avec succès depuis {file}!")
+
+#scanning_tool function
+#def scan_config():
+
+
+
+
+
+
+
+
+
+
+
+
 
 #main function
 print("Choisir une option :")
@@ -12,30 +94,16 @@ print("6 - Restaurer configuration")
 print("7 - Outils de scan")
 choice = input(str("Selectionner le numero correspondant à l'option souhaitée\n"))
 if(choice == "1"):
-    print("Ajouter une configuration")
+    add_config()
 elif(choice == "2"):
-    print("Modifier une configuration")
+    mod_config()
 elif(choice == "3"):
-    print("Supprimer une configuration")
+    del_config()
 elif(choice == "4"):
-    print("Lister les configurations")
+    ls_config()
 elif(choice == "5"):
-    print("Sauvegarder une configuration")
+    sav_config()
 elif(choice == "6"):
-    print("Restaurer une configuration")
+    rol_config
 elif(choice == "7"):
     print("Outils de scan")
-
-#add_config function
-
-#modify_config function
-
-#delete_config function
-
-#list_config function
-
-#save_config function
-
-#rollback_config function
-
-#scanning_tool function
